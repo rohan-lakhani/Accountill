44acb27836c9169e2b972a40d803ee33f43de654
RONS LAKHANI <you@example.com>
2023-04-23 11:03:40 +0530

    cors handled

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.withCredentials = true;
+
 // const API = axios.create({ baseURL: process.env.BACKEND_URL });
 
 // API.interceptors.request.use((req) => {
--- server/index.js
+++ server/index.js
@@ -27,7 +27,21 @@ const app = express();
 dotenv.config();
 
 //MIDDLEWARE
-app.use(cors());
+
+// app.use(cors({
+//     origin: "*"
+// }));
+
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+		credentials: true,
+		exposedHeaders: ["*", "Authorization"],
+	})
+);
+
 app.use(express.json({ limit: "30mb", extended: true}));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "30mb", extended: true}));
