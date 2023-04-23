0482d63dfbb9f8ff0c44b3f85d68d0c41bce1417
ROHAN171717 <rohanlakhani2003@gmail.com>
2023-04-23 15:01:02 +0530

    register.jsx changed

--- client/src/components/Auth/Register/Register.jsx
+++ client/src/components/Auth/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
